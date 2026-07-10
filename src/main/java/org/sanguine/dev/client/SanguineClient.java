deca83352d1f994341fd12d29391032aae74d5db
InfinityFarzad <10farzad.karami@gmail.com>
2026-07-10 23:17:28 +0330

    Initial commit

---
+++ src/main/java/org/sanguine/dev/client/SanguineClient.java
@@ -0,0 +1,10 @@
+package org.sanguine.dev.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SanguineClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
